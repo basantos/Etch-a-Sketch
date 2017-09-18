aebd03ef9f41bfeb67837585bc96189af8862bc1
basantos <santosbrigitte@gmail.com>
2017-09-18 14:04:00 -0700

    Added buttons, and inking and clear mechanisms

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-$(document).ready(function(){;
-  var html = "";
-  for(let i=0; i<16; i++){
-    for(let j=0; j<16; j++){
-      html += "<div class='pixel'></div>";
-      if (j === 15){
-        html += "<br>";
-      }
+// Creates grid
+var html = "";
+for(let i=0; i<16; i++){
+  for(let j=0; j<16; j++){
+    html += "<div class='pixel'></div>";
+    if (j === 15){
+      html += "<br>";
     }
   }
-  $(".grid").html(html);
+}
+$(".grid").html(html);
+
+// Colors pixel when cursor hovers over it
+$(".pixel").mouseenter(function(){
+  var inkColor = $("input").value;
+  $(this).css("background-color","black");
 });
+
+// Clears grid when clear-button clicked
+$("#clear-button").click(function(){
+  $(".pixel").css("background-color","white");
+});
+
+// Changes "ink" color
+/*$("input").change(function(){
+  var newInkColor = $("input").value;
+  $(".pixel").css("background-color", newInkColor);
+});*/
