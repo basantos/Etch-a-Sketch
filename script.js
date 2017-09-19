4746164166d075bf1916a0e10e992b8304553edf
basantos <santosbrigitte@gmail.com>
2017-09-18 19:34:18 -0700

    Added resize mechanism and changed inking mechanism

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
-// Creates grid
-var html = "";
-for(let i=0; i<16; i++){
-  for(let j=0; j<16; j++){
-    html += "<div class='pixel'></div>";
-    if (j === 15){
-      html += "<br>";
+$(document).ready(function(){
+
+function createGrid(size){
+    var html = "";
+    for(let i=0; i<size; i++){
+      for(let j=0; j<size; j++){
+        html += "<div class='pixel'></div>";
+        if (j === size-1){
+          html += "<br>";
+        }
+      }
     }
+    $(".grid").html(html);
+    $(".pixel").css("width", 400/size);
+    $(".pixel").css("height", 400/size);
   }
-}
-$(".grid").html(html);
 
-// Colors pixel when cursor hovers over it
-$(".pixel").mouseenter(function(){
-  var inkColor = $("input").value;
-  $(this).css("background-color","black");
+  // Creates initial 16x16 grid
+  createGrid(16);
+
+  // Colors pixel when cursor hovers over it
+  $(".grid").on("mouseenter", ".pixel", function(){
+    //var inkColor = $("input").value;
+    $(this).css("background-color", "black");
+  });
+
+  // Clears grid when clear-button clicked
+  $("#clear-button").click(function(){
+    $(".pixel").css("background-color","white");
+  });
+
+  // Change grid size
+  $("#resize-button").click(function(){
+    var newSize = prompt("Enter a number up to 25:");
+    createGrid(newSize);
+  });
+
+  // Changes "ink" color
+  /*$("input").change(function(){
+    var newInkColor = $("input").value;
+    $(".pixel").css("background-color", newInkColor);
+  });*/
+
 });
-
-// Clears grid when clear-button clicked
-$("#clear-button").click(function(){
-  $(".pixel").css("background-color","white");
-});
-
-// Changes "ink" color
-/*$("input").change(function(){
-  var newInkColor = $("input").value;
-  $(".pixel").css("background-color", newInkColor);
-});*/
